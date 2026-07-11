5d10215db310a205265907c9e685e887c86dd8ec
DrexAcR <[camposrodriguez.alejandro@gmail.com]>
2026-07-11 13:11:21 +0200

    Daily leetcode

---
+++ Algorithms/14-Longest Common Prefix/longest_common_prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        common_prefix = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return common_prefix
+            common_prefix += strs[0][i]
+        return common_prefix
+    
+s = Solution()
+print(s.longestCommonPrefix(["flower","flow","flight"]))
